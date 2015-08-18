d0a83eb891d59d8046c5bbd887785a6445092399
Liang Cao <caoliang2598@hotmail.com>
2015-08-18 21:36:15 +0200

    fix code style comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,40 +1,39 @@
-__author__ = 'caoliang2598'
-
 from media import Movie
 from fresh_tomatoes import open_movies_page
 
 # Movie Objects
 mad_max = Movie("Mad Max: Fury Road",
-                "In a stark desert landscape where humanity is broken, two "
-                "rebels just might be able to restore order: Max, a man of "
-                "action and of few words, and Furiosa, a woman of action who "
-                "is looking to make it back to her childhood homeland.",
+                "In a stark desert landscape where humanity is broken, two \
+                rebels just might be able to restore order: Max, a man of \
+                action and of few words, and Furiosa, a woman of action who \
+                is looking to make it back to her childhood homeland.",
                 "http://ia.media-imdb.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_SY317_CR2,0,214,317_AL_.jpg",
                 "https://www.youtube.com/watch?v=hEJnMQG9ev8")
 
 san_andreas = Movie("San Andreas",
-                    "In the aftermath of a massive earthquake in California, "
-                    "a rescue-chopper pilot makes a dangerous journey across "
-                    "the state in order to rescue his daughter.",
+                    "In the aftermath of a massive earthquake in California, \
+                    a rescue-chopper pilot makes a dangerous journey across \
+                    the state in order to rescue his daughter.",
                     "http://ia.media-imdb.com/images/M/MV5BNjI4MTgyOTAxOV5BMl5BanBnXkFtZTgwMjQwOTA4NTE@._V1_SX214_AL_.jpg",
                     "https://www.youtube.com/watch?v=23VflsU3kZE")
 
 avengers_age_of_ultron = Movie("Avengers: Age of Ultron",
-                               "When Tony Stark and Bruce Banner try to "
-                               "jump-start a dormant peacekeeping program "
-                               "called Ultron, things go horribly wrong and "
-                               "it's up to Earth's Mightiest Heroes to stop "
-                               "the villainous Ultron from enacting his "
-                               "terrible plans.",
+                               "When Tony Stark and Bruce Banner try to \
+                               jump-start a dormant peacekeeping program \
+                               called Ultron, things go horribly wrong and \
+                               it's up to Earth's Mightiest Heroes to stop \
+                               the villainous Ultron from enacting his \
+                               terrible plans.",
                                "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
                                "https://www.youtube.com/watch?v=tmeOjFno6Do")
 
 ted_second = Movie("Ted 2",
-                   "Newlywed couple Ted and Tami-Lynn want to have a baby, "
-                   "but in order to qualify to be a parent, Ted will have to "
-                   "prove he's a person in a court of law.",
+                   "Newlywed couple Ted and Tami-Lynn want to have a baby, \
+                   but in order to qualify to be a parent, Ted will have to \
+                   prove he's a person in a court of law.",
                    "http://ia.media-imdb.com/images/M/MV5BMjEwMDg3MDk1NF5BMl5BanBnXkFtZTgwNjYyODA1NTE@._V1_SX214_AL_.jpg",
                    "https://www.youtube.com/watch?v=S3AVcCggRnU")
+
 
 # Return movie db
 def get_movie_db():
