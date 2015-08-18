55b1e175e894ef280ce1be5287935f647d2a927e
Liang Cao <caoliang2598@hotmail.com>
2015-08-18 21:42:27 +0200

    fix code style comments

--- media.py
+++ media.py
@@ -1,5 +1,8 @@
 __author__ = 'caoliang2598'
 
+# A class which represent Movie.
+# it includes movie's title, poster image url, youtube trailer url, story line
+
 
 class Movie():
     def __init__(self, title, story_line, poster_image_url,
